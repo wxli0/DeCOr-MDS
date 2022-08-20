73f36550279eb8e1afc740cc6b6649a603f55ab4
Wanxin Li <w328li@uwaterloo.ca>
2022-08-20 02:28:00 -0700

    added AIC outputs

--- hmp_AIC.r
+++ hmp_AIC.r
@@ -1,4 +1,5 @@
 library(MASS)
+library(nnet)
 # library(HelpersMG)
 
 Xe_df = data.frame(read.csv("outputs/hmp_Xe_dim11.txt", sep=" ", header=FALSE))
@@ -12,9 +13,13 @@ for (pheno in phenos) {
     pheno_index = grep(pheno, colnames(hmp_df))
     pheno_ind = hmp_df[, pheno_index]
     # print(paste(pheno, length(pheno_ind)))
-    model = glm(pheno_ind ~  V1+V2+V3+V4+V5+V6+V7+V8+V9+V10, data= Xe_df, family='binomial')
-    model = stepAIC(model, direction = "backward", trace=0)
-    aic = extractAIC(model, show.option=TRUE)[2]
-    print(paste("AIC for", pheno, "is:", aic))
+    glm_model = glm(pheno_ind ~  V1+V2+V3+V4+V5+V6+V7+V8+V9+V10, data=Xe_df, family='binomial')
+    glm_model = stepAIC(glm_model, direction = "backward", trace=0)
+    glm_aic = extractAIC(glm_model, show.option=TRUE)[2]
+    print(paste("GLM model AIC for", pheno, "is:", glm_aic))
 
+    mn_model = multinom(pheno_ind ~ V1+V2+V3+V4+V5+V6+V7+V8+V9+V10, data=Xe_df, trace=0)
+    mn_model = stepAIC(mn_model, direction = "backward", trace=0)
+    mn_aic = extractAIC(mn_model, show.option=TRUE)[2]
+    print(paste("MN model AIC for", pheno, "is:", mn_aic))
 }
