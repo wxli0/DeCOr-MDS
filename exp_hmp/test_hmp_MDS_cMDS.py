05899f6b9127856e9e9a361b9f47fce1e75f51c1
Wanxin Li <w328li@uwaterloo.ca>
2022-10-03 20:52:44 -0700

    added subset outputs

--- exp_hmp/test_hmp_MDS_cMDS.py
+++ exp_hmp/test_hmp_MDS_cMDS.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import os
 import pandas as pd
 import sys
 import random as alea
@@ -9,64 +10,76 @@ from scipy.spatial.distance import pdist, squareform
 from sklearn.manifold import MDS
 
 
-
 """ Run nSimplices on HMP dataset """
-
 colors = np.loadtxt("./data/hmp_colors.txt", dtype="str")
 exec(open("./nsimplices.py").read())
 alea.seed(42)
 
-# """ QE normalization + MDS + cMDS """ 
-# dir="./data/"
-# data_path = dir+"hmp_v13lqphylotypeQuantE_rs_c.csv"
+""" QE normalization + MDS + cMDS """ 
+axes_output_path = "./outputs/hmp_QE_MDS_cMDS_axes.txt"
+if not os.path.exists(axes_output_path):
+    print("======== QE normalization + MDS + cMDS ========")
+    dir="./data/"
+    data_path = dir+"hmp_v13lqphylotypeQuantE_rs_c.csv"
 
-# df_hmp = np.loadtxt(data_path, delimiter=",")
-# hmp_dis_sq=squareform(pdist(df_hmp))
+    df_hmp = np.loadtxt(data_path, delimiter=",")
+    hmp_dis_sq=squareform(pdist(df_hmp))
 
+    # Plot cMDS embedding using the pairs of axis from the four most significant axes 
+    enforce_dim = 11 # enforcing dimension 11 to be consistent with QE+nsimplices+cMDS
+    embedding = MDS(n_components=enforce_dim) 
+    corr_coord = embedding.fit_transform(hmp_dis_sq)
+    corr_dis_sq=squareform(pdist(corr_coord))
+    _, _, Xe = cMDS(corr_dis_sq)
 
-# # Plot cMDS embedding using the pairs of axis from the four most significant axes 
-# enforce_dim = 11 # enforcing dimension 11 to be consistent with QE+nsimplices+cMDS
-# embedding = MDS(n_components=enforce_dim) 
-# corr_coord = embedding.fit_transform(hmp_dis_sq)
-# corr_dis_sq=squareform(pdist(corr_coord))
-# _, _, Xe = cMDS(corr_dis_sq)
+    np.savetxt(axes_output_path, Xe, fmt='%f')
 
-# np.savetxt("./outputs/hmp_QE_MDS_cMDS_axes.txt", Xe, fmt='%f')
+figure_output_path = "./outputs/hmp_QE_MDS_cMDS_0_1.png"
+if not os.path.exists(figure_output_path):
+    print("======== plot pairwise 2D plot ========")
+    Xe = np.loadtxt(axes_output_path)
+    num_axes = 3
+    for first_dim in range(num_axes):
+        for second_dim in range(first_dim+1, num_axes):
+            plt.figure()
+            for i in range(Xe.shape[0]):
+                plt.scatter(Xe[i, second_dim], Xe[i, first_dim], s=5, c=colors[i])
+            plt.legend(["QuantE"])
+            plt.savefig("./outputs/hmp_QE_MDS_cMDS_"+str(first_dim)+"_"+str(second_dim)+".png")
 
-# num_axes = 4
-
-# for first_dim in range(num_axes):
-#     for second_dim in range(first_dim+1, num_axes):
-#         plt.figure()
-#         for i in range(Xe.shape[0]):
-#             plt.scatter(Xe[i, second_dim], Xe[i, first_dim], s=5, c=colors[i])
-#         plt.legend(["QuantE"])
-#         plt.savefig("./outputs/hmp_QE_MDS_cMDS_"+str(first_dim)+"_"+str(second_dim)+".png")
+figure_output_path = "./outputs/hmp_QE_MDS_cMDS_subset_0_1.png"
+if not os.path.exists(figure_output_path):
+    print("======== plot pairwise 2D plot (subset) ========")
+    Xe = np.loadtxt(axes_output_path)
+    num_axes = 3
+    for first_dim in range(num_axes):
+        for second_dim in range(first_dim+1, num_axes):
+            plt.figure()
+            # only plot stool (blue), ears (black), throad (pink) points 
+            for i in range(Xe.shape[0]):
+                if colors[i] not in ["cornflowerblue", "black", "deeppink"]:
+                    continue
+                plt.scatter(Xe[i, second_dim], Xe[i, first_dim], s=5, c=colors[i])
+            plt.legend(["QuantE"])
+            plt.savefig("./outputs/hmp_QE_MDS_cMDS_subset_"+str(first_dim)+"_"+str(second_dim)+".png")
 
 
 """ NB normalization + MDS + cMDS """ 
-dir="./data/"
-data_path = dir+"hmp_v13lqphylotypeQuantNB_rs_c.csv"
+axes_output_path = "./outputs/hmp_NB_MDS_cMDS_axes.txt"
+if not os.path.exists("./outputs/hmp_NB_MDS_cMDS_axes.txt"):
+    print("======== NB normalization + MDS + cMDS ========")
+    dir="./data/"
+    data_path = dir+"hmp_v13lqphylotypeQuantNB_rs_c.csv"
 
-df_hmp = np.loadtxt(data_path, delimiter=",")
-hmp_dis_sq=squareform(pdist(df_hmp))
+    df_hmp = np.loadtxt(data_path, delimiter=",")
+    hmp_dis_sq=squareform(pdist(df_hmp))
 
-# run MDS
-enforce_dim = 11 # enforcing dimension 11 to be consistent with QE+nsimplices+cMDS
-embedding = MDS(n_components=enforce_dim) 
-corr_coord = embedding.fit_transform(hmp_dis_sq)
-corr_dis_sq=squareform(pdist(corr_coord))
+    # run MDS
+    enforce_dim = 11 # enforcing dimension 11 to be consistent with QE+nsimplices+cMDS
+    embedding = MDS(n_components=enforce_dim) 
+    corr_coord = embedding.fit_transform(hmp_dis_sq)
+    corr_dis_sq=squareform(pdist(corr_coord))
 
-# run cMDS to get the corrected coordinates in importance decreasing order
-_, _, Xe = cMDS(corr_dis_sq)
-np.savetxt("./outputs/hmp_NB_MDS_cMDS_axes.txt", Xe, fmt='%f')
-
-# Plot cMDS embedding using the pairs of axis from the four most significant axes 
-num_axes = 4
-for first_dim in range(num_axes):
-    for second_dim in range(first_dim+1, num_axes):
-        plt.figure()
-        for i in range(Xe.shape[0]):
-            plt.scatter(Xe[i, second_dim], Xe[i, first_dim], s=5, c=colors[i])
-        plt.legend(["QuantE"])
-        plt.savefig("./outputs/hmp_NB_MDS_cMDS_"+str(first_dim)+"_"+str(second_dim)+".png")
+    # run cMDS to get the corrected coordinates in importance decreasing order
+    _, _, Xe = cMDS(corr_dis_sq)
+    np.savetxt(axes_output_path, Xe, fmt='%f')
--- exp_hmp/test_hmp_nSimplices.py
+++ exp_hmp/test_hmp_nSimplices.py
@@ -11,6 +11,7 @@ from sklearn.manifold import MDS
 
 """ Prepare colors (only once) """
 if not os.path.exists("./data/colors.txt"):
+    print("======== loading colors ========")
     color_df = pd.read_csv("./data/v13lqphylotypePheno_QIHP.csv", header=0)
     colors = []
 
@@ -44,8 +45,32 @@ exec(open("./nsimplices.py").read())
 alea.seed(42)
 
 """ NB normalization + nSimplices + cMDS """ 
+axes_output_path = "./outputs/hmp_NB_nSimplices_cMDS_axes.txt"
+if not os.path.exists(axes_output_path):
+    print("======== NB normalization + nSimplices + cMDS ========")
+    dir="./data/"
+    data_path = dir+"hmp_v13lqphylotypeQuantNB_rs_c.csv"
+    df_hmp = np.loadtxt(data_path, delimiter=",")
+    hmp_dis_sq=squareform(pdist(df_hmp))
+
+    feature_num = 11
+    dim_start = 11
+    dim_end = feature_num
+
+    print("hmp_dis_sq shape is:", hmp_dis_sq.shape)
+    outlier_indices, subspace_dim , corr_pairwise_dis, corr_coord = nsimplices(hmp_dis_sq, feature_num, dim_start, dim_end)
+    print("subspace dimension is:", subspace_dim)
+
+    # run cMDS to get the corrected coordinates in importance decreasing order
+    _, _, Xe = cMDS(corr_pairwise_dis)
+    np.savetxt(axes_output_path, Xe, fmt='%f')
+
+
+""" QE normalization + nSimplices + cMDS """ 
+print("======== NB normalization + nSimplices + cMDS ========")
 dir="./data/"
-data_path = dir+"hmp_v13lqphylotypeQuantNB_rs_c.csv"
+data_path = dir+"hmp_v13lqphylotypeQuantE_rs_c.csv"
+
 df_hmp = np.loadtxt(data_path, delimiter=",")
 hmp_dis_sq=squareform(pdist(df_hmp))
 
@@ -57,95 +82,38 @@ print("hmp_dis_sq shape is:", hmp_dis_sq.shape)
 outlier_indices, subspace_dim , corr_pairwise_dis, corr_coord = nsimplices(hmp_dis_sq, feature_num, dim_start, dim_end)
 print("subspace dimension is:", subspace_dim)
 
-# run cMDS to get the corrected coordinates in importance decreasing order
-_, _, Xe = cMDS(corr_pairwise_dis)
-np.savetxt("./outputs/hmp_NB_nSimplices_cMDS_axes.txt", Xe, fmt='%f')
+""" (1) Plot cMDS embedding using the pairs of axis from the four most significant axes """
+va, ve, Xe = cMDS(corr_pairwise_dis)
+axes_output_path = "./outputs/hmp_QE_nSimplices_cMDS_axes.txt"
 
-# num_axes = 4
-
-# for first_dim in range(num_axes):
-#     for second_dim in range(first_dim+1, num_axes):
-#         plt.figure()
-#         for i in range(Xe.shape[0]):
-#             plt.scatter(Xe[i, second_dim], Xe[i, first_dim], s=5, c=colors[i])
-#         plt.legend(["QuantE+nSimplices"])
-#         plt.savefig("./outputs/hmp_QE_nSimplices_MDS_"+str(first_dim)+"_"+str(second_dim)+".png")
+np.savetxt(axes_output_path, Xe, fmt='%f')
 
 
-# """ QE normalization + nSimplices + cMDS """ 
-# dir="./data/"
-# data_path = dir+"hmp_v13lqphylotypeQuantE_rs_c.csv"
-
-# df_hmp = np.loadtxt(data_path, delimiter=",")
-# hmp_dis_sq=squareform(pdist(df_hmp))
-
-# feature_num = 11
-# dim_start = 11
-# dim_end = feature_num
-
-# print("hmp_dis_sq shape is:", hmp_dis_sq.shape)
-# outlier_indices, subspace_dim , corr_pairwise_dis, corr_coord = nsimplices(hmp_dis_sq, feature_num, dim_start, dim_end)
-# print("subspace dimension is:", subspace_dim)
-# Xe = corr_coord
+figure_output_path = "./outputs/hmp_QE_nSimplices_cMDS_0_1.png"
+if not os.path.exists(figure_output_path):
+    print("======== plot pairwise 2D plot ========")
+    num_axes = 3 # show pairwise 2D plot to decompose the 3D plot
+    for first_dim in range(num_axes):
+        for second_dim in range(first_dim+1, num_axes):
+            plt.figure()
+            # only plot stool (blue), ears (black), throad (pink) points 
+            for i in range(Xe.shape[0]):
+                plt.scatter(Xe[i, second_dim], Xe[i, first_dim], s=5, c=colors[i])
+            plt.legend(["QuantE+nSimplices"])
+            plt.savefig("./outputs/hmp_QE_nSimplices_cMDS_"+str(first_dim)+"_"+str(second_dim)+".png")
 
 
-# """ (0) Plot height vs. dimension and ratio vs dimension """
-
-# ### Importance of dimension correction in higher dimension - Fig.4(A) height distribution 
-
-# hcolls = []
-# num_point = hmp_dis_sq.shape[0]
-# for dim in range(dim_start, dim_end+1):
-#     heights = nsimplices_all_heights(num_point, hmp_dis_sq, dim, seed=dim+1)
-#     hcolls.append(heights)
-
-
-# ### Importance of dimension correction in higher dimension - Fig.4(B) dimensionality inference
-
-# # calculate median heights for tested dimension from start_dim to end_dim
-# h_meds = []
-# for hcoll in hcolls:
-#     h_meds.append(np.median(hcoll))
-
-# # calculate the ratio, where h_med_ratios[i] corresponds to h_meds[i-1]/h_meds[i]
-# # which is the (median height of dim (i-1+start_dim))/(median height of dim (i+start_dim))
-# h_med_ratios = []
-# for i in range(1, len(hcolls)):
-#     # print("dim", start_dim+i-1, "ratio is:", h_meds[i-1]/h_meds[i], h_meds[i-1], h_meds[i])
-#     h_med_ratios.append(h_meds[i-1]/h_meds[i])
-
-# # plot the height scatterplot and the ratios
-
-# fig, ax1 = plt.subplots()
-# color = 'tab:red'
-# ax1.set_xlabel(r'dimension tested $n$')
-# ax1.set_ylabel(r'median of heights', color = color)
-# ax1.scatter(list(range(dim_start, dim_end+1)), h_meds, color = color)
-# ax1.tick_params(axis ='y', labelcolor = color)
- 
-# # Adding Twin Axes to plot using dataset_2
-# ax2 = ax1.twinx()
- 
-# color = 'tab:green'
-# ax2.set_ylabel(r'heights median ratio: $h_{n-1}/h_n$', color = color)
-# ax2.plot(list(range(dim_start+1, dim_end+1)), h_med_ratios, color = color)
-# ax2.tick_params(axis ='y', labelcolor = color)
- 
-# # Show plot
-# plt.savefig("./outputs/hmp_QE_nSimplices_cMDS_ratio.png")
-# plt.close()
-
-# """ (1) Plot cMDS embedding using the pairs of axis from the four most significant axes """
-# va, ve, Xe = cMDS(corr_pairwise_dis)
-# np.savetxt("./outputs/hmp_QE_nSimplices_MDS_axes.txt", Xe, fmt='%f')
-
-
-# num_axes = 4
-
-# for first_dim in range(num_axes):
-#     for second_dim in range(first_dim+1, num_axes):
-#         plt.figure()
-#         for i in range(Xe.shape[0]):
-#             plt.scatter(Xe[i, second_dim], Xe[i, first_dim], s=5, c=colors[i])
-#         plt.legend(["QuantE+nSimplices"])
-#         plt.savefig("./outputs/hmp_QE_nSimplices_MDS_"+str(first_dim)+"_"+str(second_dim)+".png")
+figure_output_path = "./outputs/hmp_QE_nSimplices_cMDS_subset_0_1.png"
+if not os.path.exists(figure_output_path):
+    print("======== plot pairwise 2D plot (subset) ========")
+    num_axes = 3 # show pairwise 2D plot to decompose the 3D plot
+    for first_dim in range(num_axes):
+        for second_dim in range(first_dim+1, num_axes):
+            plt.figure()
+            # only plot stool (blue), ears (black), throad (pink) points 
+            for i in range(Xe.shape[0]):
+                if colors[i] not in ["cornflowerblue", "black", "deeppink"]:
+                    continue
+                plt.scatter(Xe[i, second_dim], Xe[i, first_dim], s=5, c=colors[i])
+            plt.legend(["QuantE+nSimplices"])
+            plt.savefig("./outputs/hmp_QE_nSimplices_cMDS_subset_"+str(first_dim)+"_"+str(second_dim)+".png")
