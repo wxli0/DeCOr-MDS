01410bd0b4cc8910f34988fa7101f72307f0199a
Wanxin Li <w328li@uwaterloo.ca>
2023-06-28 17:30:41 -0700

    updated runtime plots

--- exp_synthetic/plot_runtime.py
+++ exp_synthetic/plot_runtime.py
@@ -20,8 +20,35 @@ fig_path = os.path.join("./outputs/synthetic_dim10_N.pdf")
 if not os.path.exists(fig_path):
     num_points = [500, 1000, 1500, 2000, 2500]
     log_num_points = [log(x) for x in num_points]
-    # num_points_fifth = [500**5, 1000**5, 1500**5, 2000**5, 2500**5]
     runtimes = [64.90046525001526, 87.05577659606934, 654.4376263618469, 1114.2744002342224, 1897.8766615390778]
+    log_runtimes = [log(x) for x in runtimes]
+    plt.plot(log_num_points, log_runtimes, marker='.')
+    plt.gca().xaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
+    plt.yticks(fontsize=label_size)
+    plt.xticks(log_num_points, fontsize=label_size)
+    plt.xlabel(r'log(N)', fontsize=label_size)
+    plt.ylabel(r'log(T)', fontsize=label_size)
+    plt.tight_layout()
+    plt.savefig(fig_path)
+
+
+fig_path = os.path.join("./outputs/synthetic_dim40_S.pdf")
+if not os.path.exists(fig_path):
+    num_groups = [50, 100, 150, 200, 250]
+    runtimes = [745.1522696018219, 1417.604059934616, 2265.0143887996674, 2880.0754206180573, 3466.2295048236847]
+    plt.plot(num_groups, runtimes, marker='.')
+    plt.yticks(fontsize=label_size)
+    plt.xticks(num_groups, fontsize=label_size)
+    plt.xlabel(r'S', fontsize=label_size)
+    plt.ylabel(r'T', fontsize=label_size)
+    plt.tight_layout()
+    plt.savefig(fig_path)
+
+fig_path = os.path.join("./outputs/synthetic_dim40_N.pdf")
+if not os.path.exists(fig_path):
+    num_points = [500, 1000, 1500, 2000, 2500]
+    log_num_points = [log(x) for x in num_points]
+    runtimes = [409.8343632221222, 1417.604059934616, 3718.8268456459045, 6908.408477306366, 11258.810522317886]
     log_runtimes = [log(x) for x in runtimes]
     plt.plot(log_num_points, log_runtimes, marker='.')
     plt.gca().xaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
